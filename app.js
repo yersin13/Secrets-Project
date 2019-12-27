af1ebfb8062b0fcb94245326849ce5a8d0a18123
yersin13 <52936287+yersin13@users.noreply.github.com>
2019-12-27 17:47:39 -0500

    change port 3000 to port 5000

--- app.js
+++ app.js
@@ -281,10 +281,9 @@ app.post("/login", function (req, res) {
 
 });
 
-let port = process.env.PORT;
-if (port == null || port == ""){
-    port = 3000;
-}
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+//let port = process.env.PORT;
+//if (port == null || port == ""){
+//    port = 3000;
+//}
+app.listen(process.env.PORT || 5000);
 });
